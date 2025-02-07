0212ccc41b5d77e876bab70147591f9ea8520e5b
Déborah <deborahclerckx@gmail.com>
2025-02-07 21:55:33 +0100

    Update : Remise au propre de l'exercice User & Cocktail pour poursuivre avec Comment

---
+++ ASP-MVC/Models/Cocktail/CocktailListItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_MVC.Models.Cocktail
+{
+    public class CocktailListItem
+    {
+        [ScaffoldColumn(false)]
+        public Guid Cocktail_Id { get; set; }
+
+        [DisplayName("Cocktail : ")]
+        public string Name { get; set; }
+
+        [DisplayName("Description : ")]
+        public string? Description { get; set; }
+    }
+}
