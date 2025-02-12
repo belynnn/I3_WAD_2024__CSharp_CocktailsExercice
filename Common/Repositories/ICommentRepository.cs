3014da590429db98c667066c48d6d2a99afdd9ab
Deborah Clerckx <d.clerckx@interface3.be>
2025-02-12 14:46:50 +0100

    Update : Ajout des propriétés attendues dans le fichier ICommentRepository en lien avec CommentService (get, update, insert, ...)

--- Common/Repositories/ICommentRepository.cs
+++ Common/Repositories/ICommentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,11 @@ namespace Common.Repositories
 {
 	public interface ICommentRepository<TComment>
 	{
+		IEnumerable<TComment> GetFromUser(Guid user_id);
+		IEnumerable<TComment> GetFromCocktail(Guid cocktail_id);
+		TComment Get(Guid id);
+		Guid Insert(TComment entity);
+		void Update(Guid id, TComment entity);
+		void Delete(Guid id);
 	}
 }
