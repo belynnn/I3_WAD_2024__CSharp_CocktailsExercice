04ec30590f6b8c8360ce05be7b2186e5c90e7407
Deborah Clerckx <d.clerckx@interface3.be>
2025-02-12 15:46:05 +0100

    Update : Ajout de la procédure stockée GetById pour Comment - ATTENTION - vous devez republier la base de données !

---
+++ DB/Stored Procedures/SP_Comment_GetById.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[SP_Comment_GetById]
+	@comment_id UNIQUEIDENTIFIER
+AS
+BEGIN
+	SELECT	[Comment_Id],
+			[Title], 
+			[Content],
+			[Concern],
+			[CreatedBy],
+			[CreatedAt],
+			[Note]
+		FROM [Comment]
+	WHERE [Comment_Id] = @comment_id
+END
